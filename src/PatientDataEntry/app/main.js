821342389ad354a6f8fa7c9353ca0e8d4db37a05
Thomas Pepe <trp956s@gmail.com>
2016-06-17 21:35:03 -0500

    initial javascript code

--- src/PatientDataEntry/app/main.js
+++ src/PatientDataEntry/app/main.js
@@ -5,9 +5,52 @@
         .module('app')
         .controller('Main', main);
 
-    function main() {
-        var viewModel = this;
-        viewModel.food = 'hello world';
-    }
+    function main($scope) {
+        var viewController = createviewController(this, $scope);
+    };
 
+    function createviewController(viewController, $scope) {
+        viewController.model = viewController.model ? viewController.model : initModel();
+
+        viewController.onSave = function () {
+            viewController.upload();
+        };
+
+        viewController.onClear = function () {
+            viewController.resetModel();
+        };
+
+        viewController.upload = function () {
+            var uploadingModel = viewController.uploadModel();
+            viewController.resetModel();
+            viewController.patientList.push(uploadingModel);
+        };
+
+        viewController.resetModel = function () {
+            viewController.model = initModel();
+        };
+
+        viewController.uploadModel = function () {
+            var json = JSON.stringify(viewController.model);
+            var uploadingModel = JSON.parse(json);
+            uploadingModel.syncStatus = 'uploading';
+            //TODO: FTP            
+            setTimeout(function () {
+                uploadingModel.syncStatus = 'upload successful';
+                $scope.$applyAsync();
+            }, 5000);
+
+            return uploadingModel;
+        }
+
+        viewController.patientList = [];
+
+        return viewController;
+    };
+
+    function initModel() {
+        var model = {};
+        model.syncStatus = 'new';
+        return model;
+    }
 })();
