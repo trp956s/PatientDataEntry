a374b24832a8aa0a55ad3be2ea71b8e44520bf44
Thomas Pepe <trp956s@gmail.com>
2016-07-02 13:20:26 -0500

    Remove repetitive HTML, update readme, remove unneeded controller, unneeded settings, unneeded WebClient, and unneeded view

--- src/PatientDataEntry/Startup.cs
+++ src/PatientDataEntry/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.DotNet.InternalAbstractions;
 
 namespace PatientDataEntry
 {
@@ -21,28 +25,62 @@ namespace PatientDataEntry
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-				;
+                ;
 
             builder.AddEnvironmentVariables();
             builder.Build();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// Thanks to http://benjii.me/2016/01/angular2-routing-with-asp-net-core-1/ I 1. customise the router 
+        /// in the use method 2. support the file system by calling UseFileServer and 3. enable MVC for data requests
+        /// </summary>
+        /// <param name="build"></param>
+        public void Configure(IApplicationBuilder build)
         {
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseMvc(this.ConfigureRoutes);
+            build.Use(this.RouteAllUnknownRequestsToAppRoot);
+            build.UseFileServer();
+            build.UseMvc();
         }
 
-        private void ConfigureRoutes(IRouteBuilder routes)
+
+        /// <summary>
+        /// This method customizes the router by forwarding all requests that cannot be served by MVC to default.html
+        /// TODO: support an actual 404 page and require a login
+        /// TODO: prevent file browsing by overriding the File Server Options also seen on the same link below
+        /// Thanks to http://benjii.me/2016/01/angular2-routing-with-asp-net-core-1/ I added this method to Use
+        /// I also changed the configure file.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <returns></returns>
+        private async Task RouteAllUnknownRequestsToAppRoot(HttpContext context, Func<Task> next)
         {
-            routes.MapRoute(
-                name: "spa-fallback",
-                template: "{*url}",
-                defaults: new { controller = "Home", action = "Index" });
+            await next();
+
+            if(!this.RewriteRequestToUseAppRoute(context))
+            {
+                return;
+            }
+
+            context.Request.Path = "/default.html";
+            await next();
+        }
+
+
+        /// <summary>
+        /// Determine which requests should be replaced with default.html
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private bool RewriteRequestToUseAppRoute(HttpContext context)
+        {
+            var thereIsNoAvailableFile = context.Response.StatusCode == 404;
+            var theRequestDoesNotContainAnExtension = !Path.HasExtension(context.Request.Path.Value);
+
+            return thereIsNoAvailableFile && theRequestDoesNotContainAnExtension;
         }
     }
 }
--- src/PatientDataEntry/WebClient.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PatientDataEntry.Controllers
-{
-    internal class WebClient
-    {
-    }
-}
